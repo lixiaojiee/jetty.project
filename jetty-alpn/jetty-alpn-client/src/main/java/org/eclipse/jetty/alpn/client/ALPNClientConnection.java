6062871a10bf7cd3e4fd8fd9b56967cb178d28fa
lixiaojiee <xiaoj.l@outlook.com>
2018-12-30 01:46:27 +0800

    2121

--- jetty-alpn/jetty-alpn-client/src/main/java/org/eclipse/jetty/alpn/client/ALPNClientConnection.java
+++ jetty-alpn/jetty-alpn-client/src/main/java/org/eclipse/jetty/alpn/client/ALPNClientConnection.java
@@ -42,6 +42,10 @@ public class ALPNClientConnection extends NegotiatingClientConnection
         this.protocols = protocols;
     }
 
+    /**
+     *
+     * @return
+     */
     public List<String> getProtocols()
     {
         return protocols;
